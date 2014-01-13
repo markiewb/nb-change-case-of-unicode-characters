806a05b8f7f11c6cbbd2f3011fef5f40de090018
markiewb <benno.markiewicz@googlemail.com>
2014-01-13 22:43:27 +0100

    issue #1: Convert to maven based module

--- src/main/java/de/markiewb/netbeans/plugins/changecaseofunicodecharacters/ConvertToLowerCaseUnicodeAction.java
+++ src/main/java/de/markiewb/netbeans/plugins/changecaseofunicodecharacters/ConvertToLowerCaseUnicodeAction.java
@@ -1,13 +1,28 @@
+/*
+ * Copyright 2014 Benno Markiewicz (benno.markiewicz@googlemail.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.markiewb.netbeans.plugins.changecaseofunicodecharacters;
 
 import de.markiewb.netbeans.plugins.changecaseofunicodecharacters.Converter.ConvertMode;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import org.openide.loaders.DataObject;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionReferences;
 import org.openide.awt.ActionRegistration;
+import org.openide.loaders.DataObject;
 import org.openide.util.NbBundle.Messages;
 
 @ActionID(
@@ -32,7 +47,6 @@ public final class ConvertToLowerCaseUnicodeAction implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent ev) {
-        ConvertMode codeMode = ConvertMode.LOWERCASE;
         new Converter().convertUnicodeCharacters(context.getPrimaryFile(), ConvertMode.LOWERCASE);
     }
 }
